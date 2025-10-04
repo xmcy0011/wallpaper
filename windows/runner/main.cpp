b4e06d8b99786ebc24c53275730387b3c04be3a4
xmcy0011 <xmcy0011@sina.com>
2025-10-04 14:43:52 +0800

    init

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,6 +5,10 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
@@ -27,7 +31,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"wallpaper", origin, size)) {
+  if (!window.Create(L"壁纸APP", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
